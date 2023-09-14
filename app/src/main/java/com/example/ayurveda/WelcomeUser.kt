6e457def19bdd32cb612d8f1ffe60ba0f633d8cd
Imesh Pasinda <imeshpasinda@gmail.com>
2023-09-14 15:00:01 +0530

    Created WelcomeUser screen

--- app/src/main/java/com/example/ayurveda/WelcomeUser.kt
+++ app/src/main/java/com/example/ayurveda/WelcomeUser.kt
@@ -1,5 +1,6 @@
 package com.example.ayurveda
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class WelcomeUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_user)
+
+        // Customize the status bar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.statusBarColor = getColor(R.color.white)
+        }
     }
 }
