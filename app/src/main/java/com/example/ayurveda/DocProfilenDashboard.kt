6fd0d4c4d1a50db98f4aff8f7fe849340a526e2a
Darshi Buddhini <darshibuddhini@gmail.com>
2023-10-14 12:23:45 +0530

    Doctor pages UI created

---
+++ app/src/main/java/com/example/ayurveda/DocProfilenDashboard.kt
@@ -0,0 +1,11 @@
+package com.example.ayurveda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DocProfilenDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doc_profilen_dashboard)
+    }
+}
---
+++ app/src/main/java/com/example/ayurveda/WelcomeDoctor.kt
@@ -0,0 +1,11 @@
+package com.example.ayurveda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeDoctor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_doctor)
+    }
+}
