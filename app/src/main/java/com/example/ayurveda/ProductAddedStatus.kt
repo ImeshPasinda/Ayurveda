a781c3ce352045c7efce315b9ea11a00d377752c
Kimuthu Gamage <120770157+kimuthuUG@users.noreply.github.com>
2023-10-17 09:33:24 +0530

    latest update

---
+++ app/src/main/java/com/example/ayurveda/ProductAddedStatus.kt
@@ -0,0 +1,11 @@
+package com.example.ayurveda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductAddedStatus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_added_status)
+    }
+}
