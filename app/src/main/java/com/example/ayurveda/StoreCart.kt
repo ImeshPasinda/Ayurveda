bd8f6360d5577265fc7fe4a60d49f19a2f93dfb7
Imesh Pasinda <imeshpasinda@gmail.com>
2023-10-19 15:40:30 +0530

    Created Functions in Herbal Product Directory and Online Ordering v1.0

--- app/src/main/java/com/example/ayurveda/StoreCart.kt
+++ app/src/main/java/com/example/ayurveda/StoreCart.kt
@@ -1,11 +1,15 @@
 package com.example.ayurveda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 
 class StoreCart : AppCompatActivity() {
     private var quantity = 1 // Initialize quantity to 1
@@ -49,5 +53,46 @@ class StoreCart : AppCompatActivity() {
             val totalPriceTextView = findViewById<TextView>(R.id.totprice)
             totalPriceTextView.text = String.format("%.2f", total)
         }
+
+
+
+        // Navbar
+        val sessionManager = SessionManager(this)
+        val userEmail = sessionManager.getUserEmail()
+
+        val db = FirebaseFirestore.getInstance()
+
+        val userNavButton = findViewById<ImageButton>(R.id.userProfileNavbtn)
+        userNavButton.setOnClickListener {
+            val intent = Intent(this, UserProfile::class.java)
+            startActivity(intent)
+        }
+        // Retrieve the username from the "users" collection
+        val usersCollection = db.collection("users")
+        usersCollection.whereEqualTo("email", userEmail)
+            .get()
+            .addOnSuccessListener { userQuerySnapshot ->
+                if (!userQuerySnapshot.isEmpty) {
+                    // There may be multiple documents matching the email; loop through them if needed
+                    for (userDocument in userQuerySnapshot.documents) {
+                        val username = userDocument.getString("username")
+
+                        // Now you have the username, and you can use it as needed
+                        // For example, you can set it in a TextView
+                        val usernameTextView = findViewById<TextView>(R.id.unamenavbar)
+                        usernameTextView.text = username?.split(" ")?.get(0) ?: "User"
+
+                        // If you found the username you were looking for, you can break out of the loop
+                        break
+                    }
+                } else {
+                    // Handle the case when no document with the matching email is found
+                    Log.d("Firestore", "No user document found for email: $userEmail")
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle the failure to retrieve user data
+                Log.e("Firestore", "Error getting user document: $exception")
+            }
     }
 }
