5fd4e3dfad74a01260aad398082692a49550964a
Imesh Pasinda <imeshpasinda@gmail.com>
2023-10-28 11:21:27 +0530

    Finalize Processing v1.0

--- app/src/main/java/com/example/ayurveda/AddProduct.kt
+++ app/src/main/java/com/example/ayurveda/AddProduct.kt
@@ -43,69 +43,55 @@ class AddProduct : AppCompatActivity() {
 
 
 
-        //Navbar
-
+        // Navbar
         val sessionManager = SessionManager(this)
-        val userEmail = sessionManager.getUserEmail()
+        userEmail = sessionManager.getUserEmail().toString()
 
-        //Profile
+
+        val db = FirebaseFirestore.getInstance()
+
         val userNavButton = findViewById<ImageButton>(R.id.userProfileNavbtn)
         userNavButton.setOnClickListener {
             val intent = Intent(this, UserProfile::class.java)
             startActivity(intent)
         }
-        //Home
-        val homeBtn = findViewById<ImageButton>(R.id.homenavbtn_1)
-        homeBtn.setOnClickListener {
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
-        }
-        //Appointments
-        val userAppointmentButton = findViewById<ImageButton>(R.id.userAppointmentsbtn)
-        userAppointmentButton.setOnClickListener {
-            val intent = Intent(this, UserAppointments::class.java)
-            startActivity(intent)
-        }
-        //Store
-        val userStoreButton = findViewById<ImageButton>(R.id.storenavbtn)
-        userStoreButton.setOnClickListener {
-            val intent = Intent(this, StoreHome::class.java)
-            startActivity(intent)
-        }
-        //Remedy
-        val RemedyButton = findViewById<ImageButton>(R.id.remedyNavBtn)
-        RemedyButton.setOnClickListener {
-            val intent = Intent(this, DoctorRecommendation::class.java)
-            startActivity(intent)
-        }
 
-        // Retrieve the username from the "users" collection
         val usersCollection = db.collection("users")
         usersCollection.whereEqualTo("email", userEmail)
             .get()
             .addOnSuccessListener { userQuerySnapshot ->
                 if (!userQuerySnapshot.isEmpty) {
-                    // There may be multiple documents matching the email; loop through them if needed
                     for (userDocument in userQuerySnapshot.documents) {
                         val username = userDocument.getString("username")
 
-                        // Now you have the username, and you can use it as needed
-                        // For example, you can set it in a TextView
                         val usernameTextView = findViewById<TextView>(R.id.unamenavbar)
                         usernameTextView.text = username?.split(" ")?.get(0) ?: "User"
 
-                        // If you found the username you were looking for, you can break out of the loop
                         break
                     }
                 } else {
-                    // Handle the case when no document with the matching email is found
                     Log.d("Firestore", "No user document found for email: $userEmail")
                 }
             }
             .addOnFailureListener { exception ->
-                // Handle the failure to retrieve user data
                 Log.e("Firestore", "Error getting user document: $exception")
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -193,7 +179,7 @@ class AddProduct : AppCompatActivity() {
             productsCollection.add(productMap)
                 .addOnSuccessListener { documentReference ->
                     val productId = documentReference.id
-                    showToast("Product added successfully with ID: $productId")
+                    showToast("Product added successfully!")
                     // Navigate to SuccessActivity and pass data
                     val successIntent = Intent(this, ProductAddedStatus::class.java)
                     successIntent.putExtra("refNo", productId)
