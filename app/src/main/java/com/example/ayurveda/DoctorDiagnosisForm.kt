3b2951bbc39248af6565a9849b6d0199e2a6e56f
Kimuthu Gamage <120770157+kimuthuUG@users.noreply.github.com>
2023-10-28 10:54:20 +0530

    Updated DoctorDiagnosisForm

--- app/src/main/java/com/example/ayurveda/DoctorDiagnosisForm.kt
+++ app/src/main/java/com/example/ayurveda/DoctorDiagnosisForm.kt
@@ -1,4 +1,5 @@
 package com.example.ayurveda
+
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.View
@@ -14,7 +15,6 @@ import com.google.firebase.firestore.SetOptions
 
 class DoctorDiagnosisForm : AppCompatActivity() {
 
-
     private lateinit var patientName: String
     private lateinit var bookingDate: String
     private lateinit var bookingTime: String
@@ -26,47 +26,16 @@ class DoctorDiagnosisForm : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
     private lateinit var documentId: String
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_diagnosis_form)
 
-        val sessionManager = SessionManager(this)
-        val userEmail = sessionManager.getUserEmail()
-
-        // Retrieve the username from the "users" collection
-        val usersCollection = db.collection("doctors")
-        usersCollection.whereEqualTo("email", userEmail)
-            .get()
-            .addOnSuccessListener { userQuerySnapshot ->
-                if (!userQuerySnapshot.isEmpty) {
-                    // There may be multiple documents matching the email; loop through them if needed
-                    for (userDocument in userQuerySnapshot.documents) {
-                        val username = userDocument.getString("docNameEn")
-
-                        // Now you have the username, and you can use it as needed
-                        // For example, you can set it in a TextView
-                        val usernameTextView = findViewById<TextView>(R.id.unamenavbar)
-                        usernameTextView.text = "Dr. " + (username?.split(" ")?.get(0) ?: "Doctor")
-
-                        // If you found the username you were looking for, you can break out of the loop
-                        break
-                    }
-                } else {
-                    // Handle the case when no document with the matching email is found
-                    Log.d("Firestore", "No user document found for email: $userEmail")
-                }
-            }
-            .addOnFailureListener { exception ->
-                // Handle the failure to retrieve user data
-                Log.e("Firestore", "Error getting user document: $exception")
-            }
-
         // Retrieve data passed from the previous activity
         patientName = intent.getStringExtra("patientName") ?: ""
         bookingDate = intent.getStringExtra("bookingDate") ?: ""
         bookingTime = intent.getStringExtra("bookingTime") ?: ""
-
 
         val date = findViewById<TextView>(R.id.datetxtapp)
         date.text = bookingDate
@@ -89,14 +58,16 @@ class DoctorDiagnosisForm : AppCompatActivity() {
 
             // Prepare data to be stored in Firestore
             val diagnosisData = hashMapOf(
-
                 "symptoms" to symptoms,
                 "medication" to medication,
                 "notes" to notes,
                 "status" to "completed"
                 // Add more fields as needed
             )
+
+            // Check if there's an existing document ID
             if (documentId.isNotEmpty()) {
+                // Update the document in the "appointments" collection
                 db.collection("appointments")
                     .document(documentId)
                     .set(diagnosisData, SetOptions.merge())
@@ -108,15 +79,16 @@ class DoctorDiagnosisForm : AppCompatActivity() {
                         intent.putExtra("symptoms", symptoms)
                         intent.putExtra("medication", medication)
                         intent.putExtra("notes", notes)
-
-
                         startActivity(intent)
                     }
                     .addOnFailureListener { e ->
                         // Handle errors
+                        showToast("Failed to save diagnosis")
                     }
             }
         }
+
+        // Query the Firestore database to find the matching document in the "appointments" collection
         db.collection("appointments")
             .whereEqualTo("patientName", patientName)
             .whereEqualTo("bookingDate", bookingDate)
@@ -125,13 +97,25 @@ class DoctorDiagnosisForm : AppCompatActivity() {
             .addOnSuccessListener { documents ->
                 if (!documents.isEmpty) {
                     // Assuming there is only one matching document
-                    documentId = documents.documents[0].id
+                    val document = documents.documents[0]
+                    documentId = document.id
+
+                    // Retrieve relevant data from the document and set it to the UI fields
+                    val appointmentData = document.data
+                    if (appointmentData != null) {
+                        symptomsEditText.setText(appointmentData["symptoms"].toString())
+                        medicationEditText.setText(appointmentData["medications"].toString())
+                        notesEditText.setText(appointmentData["notes"].toString())
+                    }
                 }
+            }
+            .addOnFailureListener { e ->
+                // Handle errors
+                showToast("Failed to retrieve appointment data")
             }
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-
 }
