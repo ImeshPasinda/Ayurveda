f6bae07d00985e20c417272d528266671a3b9d2e
Imesh Pasinda <imeshpasinda@gmail.com>
2023-10-28 12:06:25 +0530

    Finalize Processing v1.1

--- app/src/main/java/com/example/ayurveda/Remedies.kt
+++ app/src/main/java/com/example/ayurveda/Remedies.kt
@@ -1,11 +1,63 @@
 package com.example.ayurveda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Remedies : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remedies)
+
+        val db = FirebaseFirestore.getInstance()
+
+        // val category = intent.getStringExtra("category")
+        val description = intent.getStringExtra("description")
+        val doctorName = intent.getStringExtra("doctorName")
+        val title = intent.getStringExtra("title")
+
+        val subTitleTextView = findViewById<TextView>(R.id.remedySubTitle)
+        val descriptionTextView = findViewById<TextView>(R.id.remedydescpage)
+        val doctorNameTextView = findViewById<TextView>(R.id.docNamePtxt9)
+        val titleTextView = findViewById<TextView>(R.id.remedyTitle)
+
+        subTitleTextView.text = title
+        descriptionTextView.text = description
+        doctorNameTextView.text = doctorName
+        titleTextView.text = title
+
+        val sessionManager = SessionManager(this)
+        val userEmail = sessionManager.getUserEmail()
+
+        val userNavButton = findViewById<ImageButton>(R.id.userProfileNavbtn)
+        userNavButton.setOnClickListener {
+            val intent = Intent(this, UserProfile::class.java)
+            startActivity(intent)
+        }
+
+        val usersCollection = db.collection("users")
+        usersCollection.whereEqualTo("email", userEmail)
+            .get()
+            .addOnSuccessListener { userQuerySnapshot ->
+                if (!userQuerySnapshot.isEmpty) {
+                    for (userDocument in userQuerySnapshot.documents) {
+                        val username = userDocument.getString("username")
+                        val usernameTextView = findViewById<TextView>(R.id.unamenavbar)
+                        usernameTextView.text = username?.split(" ")?.get(0) ?: "User"
+                        break
+                    }
+                } else {
+                    Log.d("Firestore", "No user document found for email: $userEmail")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("Firestore", "Error getting user document: $exception")
+            }
     }
 }
--- app/src/main/java/com/example/ayurveda/RemedyAdapter.kt
+++ app/src/main/java/com/example/ayurveda/RemedyAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.ayurveda
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ayurveda.R
@@ -24,6 +26,17 @@ class RemedyAdapter(private var remediesList: List<Remedy>) :
         // Bind data to the views
         holder.remedyTitle.text = remedy.title
         holder.remedyDocName.text = remedy.doctorName
+
+        holder.profileBtn.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, Remedies::class.java)
+
+            intent.putExtra("description", remedy.description)
+            intent.putExtra("doctorName", remedy.doctorName)
+            intent.putExtra("title", remedy.title)
+
+            context.startActivity(intent)
+        }
     }
 
     fun updateData(newData: List<Remedy>) {
@@ -38,5 +51,6 @@ class RemedyAdapter(private var remediesList: List<Remedy>) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val remedyTitle: TextView = itemView.findViewById(R.id.remedyTitle)
         val remedyDocName: TextView = itemView.findViewById(R.id.remedyDocName)
+        val profileBtn: ImageButton = itemView.findViewById(R.id.goRemedyProfile)
     }
 }
